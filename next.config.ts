e7b2d21b4f3587cb940701b08d4b851309c77362
t3vada prime <149129078+t3vada@users.noreply.github.com>
2025-11-06 10:57:29 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import type { NextConfig } from "next";
  */
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/basePath"
+  basePath: "/portfolio"
 };
 
 export default nextConfig;
